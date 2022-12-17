f7b491810a9f14f2a9d7642eb39f99a112b8c15f
shulme801 <shulme801@gmail.com>
2022-12-17 15:20:06 -0500

    12/17: Commit 1

--- Chapter2/platform_test.py
+++ Chapter2/platform_test.py
@@ -1,6 +1,8 @@
-from platform import platform,machine
+from platform import platform,machine,processor,system
 
 print(platform())
 print(platform(1))
 print(platform(0, 1))
 print(machine())
+print(processor())
+print(system())
